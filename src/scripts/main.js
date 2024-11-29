cb4712cd4ed5ae54e20df91038ea51295c3ecc28
Aleksandr Shramchenko <mostwar1988@gmail.com>
2024-11-29 11:27:54 +0200

    change the className

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,20 +2,21 @@
 
 const divElement = document.getElementsByClassName('field');
 
-
-
-for (let i = 0; i < divElement.length; i++){
+for (let i = 0; i < divElement.length; i++) {
   const labName = divElement[i].firstElementChild.getAttribute('name');
 
-  let massLbName = labName.split('');
-  massLbName[0] = massLbName[0].toUpperCase();
-  const str = massLbName.join('');
+  const nameArray = labName.split('');
+
+  nameArray[0] = nameArray[0].toUpperCase();
+
+  const str = nameArray.join('');
 
   divElement[i].firstElementChild.setAttribute('placeholder', str);
 
-  const lab = document.createElement("label");
+  const lab = document.createElement('label');
+
   lab.innerHTML = labName.toUpperCase();
-  lab.setAttribute('for',  labName);
+  lab.setAttribute('for', labName);
 
   divElement[i].prepend(lab);
 }
