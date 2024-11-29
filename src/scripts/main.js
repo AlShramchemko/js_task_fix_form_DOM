b4da5c6ee4c6b5cb90a35eb79bf008a208b1a82b
Aleksandr Shramchenko <mostwar1988@gmail.com>
2024-11-29 11:24:27 +0200

    complite

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,21 @@
 'use strict';
 
-// write code here
+const divElement = document.getElementsByClassName('field');
+
+
+
+for (let i = 0; i < divElement.length; i++){
+  const labName = divElement[i].firstElementChild.getAttribute('name');
+
+  let massLbName = labName.split('');
+  massLbName[0] = massLbName[0].toUpperCase();
+  const str = massLbName.join('');
+
+  divElement[i].firstElementChild.setAttribute('placeholder', str);
+
+  const lab = document.createElement("label");
+  lab.innerHTML = labName.toUpperCase();
+  lab.setAttribute('for',  labName);
+
+  divElement[i].prepend(lab);
+}
